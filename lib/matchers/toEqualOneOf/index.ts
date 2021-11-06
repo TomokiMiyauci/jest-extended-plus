f759b7ba23665ccb7232134da3e2f8179c63ae7f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-06 17:28:45 +0900

    :bug:  Fix matcher interface

--- lib/matchers/toEqualOneOf/index.ts
+++ lib/matchers/toEqualOneOf/index.ts
@@ -1,28 +1,23 @@
-import type { RawMatcherFn, SyncExpectationResult } from 'expect/build/types'
 import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils'
 import { predict } from '@/matchers/toEqualOneOf/predict'
 
-const passMessage =
-  (item: unknown, list: unknown[]): SyncExpectationResult['message'] =>
-  () =>
-    matcherHint('.not.toEqualOneOf', 'item', 'list') +
-    '\n\n' +
-    'Expected value to not be in list:\n' +
-    `  ${printExpected(list)}\n` +
-    'Received:\n' +
-    `  ${printReceived(item)}`
+const passMessage = (item: unknown, list: unknown[]) => () =>
+  matcherHint('.not.toEqualOneOf', 'item', 'list') +
+  '\n\n' +
+  'Expected value to not be in list:\n' +
+  `  ${printExpected(list)}\n` +
+  'Received:\n' +
+  `  ${printReceived(item)}`
 
-const failMessage =
-  (item: unknown, list: unknown[]): SyncExpectationResult['message'] =>
-  () =>
-    matcherHint('.toEqualOneOf', 'item', 'list') +
-    '\n\n' +
-    'Expected value to be in list:\n' +
-    `  ${printExpected(list)}\n` +
-    'Received:\n' +
-    `  ${printReceived(item)}`
+const failMessage = (item: unknown, list: unknown[]) => () =>
+  matcherHint('.toEqualOneOf', 'item', 'list') +
+  '\n\n' +
+  'Expected value to be in list:\n' +
+  `  ${printExpected(list)}\n` +
+  'Received:\n' +
+  `  ${printReceived(item)}`
 
-const toEqualOneOf: RawMatcherFn = (received, expected: unknown[]) => {
+const toEqualOneOf = (received: any, expected: unknown[]) => {
   const pass = predict(received, expected)
 
   return {
