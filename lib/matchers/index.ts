2009a2e502355327ea84141be840f0e10ab3f74c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-06 20:48:29 +0900

    :bug:  Export martchers

--- lib/matchers/index.ts
+++ lib/matchers/index.ts
@@ -2,18 +2,38 @@ import { toEqualOneOf } from '@/matchers/toEqualOneOf'
 import { toBeNumberOrNull } from '@/matchers/toBeNumberOrNull'
 import { toBeStringOrNull } from '@/matchers/toBeStringOrNull'
 
-type TailFn<T extends (...args: any) => any> = (
-  args: Parameters<T> extends [infer _, infer Rest] ? Rest : Parameters<T>
-) => ReturnType<T>
+type TailFn<T extends (...args: any) => any, R> = Parameters<T> extends [
+  infer _,
+  infer Rest
+]
+  ? (args: Rest) => R
+  : () => R
 
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toEqualOneOf: TailFn<typeof toEqualOneOf>
-      toBeNumberOrNull: TailFn<typeof toBeNumberOrNull>
-      toBeStringOrNull: TailFn<typeof toBeStringOrNull>
+      /**
+       * Use `.toEqualOneOf` when checking if a value equal to a member of a given `Array`
+       *
+       * @beta
+       */
+      toEqualOneOf: TailFn<typeof toEqualOneOf, R>
+
+      /**
+       * Use `.toBeNumberOrNull` when checking if a value is a `number` or `null`
+       *
+       * @beta
+       */
+      toBeNumberOrNull: TailFn<typeof toBeNumberOrNull, R>
+
+      /**
+       * Use `.toBeStringOrNull` when checking if a value is a `string` or `null`
+       *
+       * @beta
+       */
+      toBeStringOrNull: TailFn<typeof toBeStringOrNull, R>
     }
   }
 }
 
-export { toEqualOneOf }
+export { toEqualOneOf, toBeStringOrNull, toBeNumberOrNull }
