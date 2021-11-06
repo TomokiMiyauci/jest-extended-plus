8ce378b1cab67d3cae1c2776a81facd1ea509fbd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-06 19:31:41 +0900

    :bug:  Fix types of toEqualOneOf

--- lib/matchers/index.ts
+++ lib/matchers/index.ts
@@ -1,9 +1,13 @@
 import { toEqualOneOf } from '@/matchers/toEqualOneOf'
 
+type TailFn<T extends (...args: any) => any> = (
+  args: Parameters<T> extends [infer _, infer Rest] ? Rest : Parameters<T>
+) => ReturnType<T>
+
 declare global {
   namespace jest {
     interface Matchers<R> {
-      toEqualOneOf: typeof toEqualOneOf
+      toEqualOneOf: TailFn<typeof toEqualOneOf>
     }
   }
 }
